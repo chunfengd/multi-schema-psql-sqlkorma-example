c5986de9f8db1010203fe480e3eb8a6dfd2d9d2b
chunfeng <chunfeng@xiaohuihui>
2013-12-20 11:38:17 -0500

    minor

--- dump.rb
+++ dump.rb
@@ -2,15 +2,16 @@
 per_user_table = "per_user_new.sql"
 user_table = "users_new.sql"
 @dump_path = "/tmp"
+@psql = "psql"
 
 def copy(tablename, columns, f, id)
   `sqlite3 -csv -noheader #{f} "select #{columns} from #{tablename}" | grep -v '^$' > #{@dump_path}/#{id}-#{tablename}.csv`
-  `echo "COPY u#{id}.#{tablename} (#{columns}) FROM '#{@dump_path}/#{id}-#{tablename}.csv' DELIMITER ',' CSV;" | psql`
+  `echo "COPY u#{id}.#{tablename} (#{columns}) FROM '#{@dump_path}/#{id}-#{tablename}.csv' DELIMITER ',' CSV;" | #{@psql}`
 end
 
 def copyuser(tablename, columns, f)
   `sqlite3 -csv -noheader #{f} "select #{columns} from #{tablename}" | grep -v '^$' > #{@dump_path}/#{tablename}.csv`
-  `echo "COPY #{tablename} (#{columns}) FROM '#{@dump_path}/#{tablename}.csv' DELIMITER ',' CSV;" | psql`
+  `echo "COPY #{tablename} (#{columns}) FROM '#{@dump_path}/#{tablename}.csv' DELIMITER ',' CSV;" | #{@psql}`
 end
 
 fs = Dir["*.db"]
@@ -20,7 +21,7 @@ fs.each do |f|
   if id.match(/[0-9]+/)
     puts id
     # create table
-    `echo "DROP SCHEMA IF EXISTS u#{id} CASCADE;CREATE SCHEMA u#{id};\nSET search_path TO u#{id},public;" | cat - #{per_user_table} | psql`
+    `echo "DROP SCHEMA IF EXISTS u#{id} CASCADE;CREATE SCHEMA u#{id};\nSET search_path TO u#{id},public;" | cat - #{per_user_table} | #{@psql}`
 
     # twitter_users
     columns = "id, at, followed_at, followed_date, unfollowed_at, unfollowed_date, flag"
@@ -43,8 +44,8 @@ fs.each do |f|
     copy("follow_scores", columns, f, id)
   elsif (id == "users")
     puts id
-    `echo "drop table if exists users; drop table if exists phrases; drop table if exists competitors" | psql`
-    `cat #{user_table} | psql`
+    `echo "drop table if exists users; drop table if exists phrases; drop table if exists competitors" | #{@psql}`
+    `cat #{user_table} | #{@psql}`
 
     # users
     columns = "id, referred_from_id, at, status, screen_name, email, usertoken, usersecret, apptoken, appsecret, json, daily_follow_limit, friends_followers_ratio, unfollow_days"
